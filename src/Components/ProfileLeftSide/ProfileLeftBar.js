ec0a5d48bb12729ebf94ca013e86f6829c995522
justina177 <justinaenenche@yahoo.com>
2023-04-12 20:36:00 +0100

    updated the profileLeftBar component

--- src/Components/ProfileLeftSide/ProfileLeftBar.js
+++ src/Components/ProfileLeftSide/ProfileLeftBar.js
@@ -16,22 +16,22 @@ export default function ProfileLeftBar() {
                                          <div style={{display:'flex' , alignItems:'center' , marginTop:-30}}>
                                                   <img src={`${image2}`} className="Profilepageimage" alt="" />
                                                   <div>
-                                                    <p style={{marginLeft:6 , marginTop:20 , color:"black" , textAlign:'start'}}>Suman</p>
+                                                    <p style={{marginLeft:6 , marginTop:20 , color:"black" , textAlign:'start'}}>Chris </p>
                                                     <p style={{marginLeft:6 , marginTop:20 , color:"black" , textAlign:"start"  , fontSize:11}}>Software Developer</p>
                                                   </div>
                                          </div>
                                          <div style={{display:'flex' , justifyContent:'space-between'}}>
                                                   <p style={{color:"black", marginLeft:20 , fontSize:"14px"}}>Profile Views</p>
-                                                  <p style={{color:"black" , marginRight:20 , fontSize:"12px" , marginTop:17}}>43924</p>
+                                                  <p style={{color:"black" , marginRight:20 , fontSize:"12px" , marginTop:17}}>76,876</p>
                                          </div>
 
                                          <div style={{display:'flex' , justifyContent:'space-between' , marginTop:-20}}>
                                                   <p style={{color:"black", marginLeft:20 , fontSize:"14px"}}>Friends</p>
-                                                  <p style={{color:"black" , marginRight:20 , fontSize:"12px" , marginTop:17}}>4324</p>
+                                                  <p style={{color:"black" , marginRight:20 , fontSize:"12px" , marginTop:17}}>4,209</p>
                                          </div>
                                          <div style={{marginTop:-20}}>
                                                   <h5 style={{color:"black", marginLeft:10 , fontSize:"14px" , marginRight:30, marginTop:30 , textAlign:"start"}}>User bio</h5>
-                                                  <p style={{color:"black"  , fontSize:"12px" , marginTop:-20 , textAlign:"start" , marginLeft:"10px"}}>I would rather be despised of who I am, rather than loved by who I am not.</p>
+                                                  <p style={{color:"#aaae"  , fontSize:"12px" , marginTop:-20 , textAlign:"start" , marginLeft:"10px"}}>Nothing comes as an accomplishment instantly. Success does not come overnight. Patience is the key!. Remember great things take time.</p>
                                          </div>
                                          <button style={{width:"100%" , paddingTop:7 , paddingBottom:7 , border:"none" , backgroundColor:"green" , color:"white"}}>Edit Bio</button>
                                         
@@ -50,23 +50,23 @@ export default function ProfileLeftBar() {
                                         </div>
                                         <div style={{marginLeft:4}}>
                                                   <img src={`${image1}`} className="friendimage" alt="" />
-                                                  <p style={{marginTop:-2}}>Samn</p>
+                                                  <p style={{marginTop:-2}}>Samuel</p>
                                         </div>
                                         <div style={{marginLeft:4}}>
                                                   <img src={`${image5}`} className="friendimage" alt="" />
-                                                  <p style={{marginTop:-2}}>Uman</p>
+                                                  <p style={{marginTop:-2}}>Usher</p>
                                         </div>
                                         <div style={{marginLeft:4}}>
                                                   <img src={`${image3}`} className="friendimage" alt="" />
-                                                  <p style={{marginTop:-2}}>Dan gAN</p>
+                                                  <p style={{marginTop:-2}}>Dennis</p>
                                         </div>
                                         <div style={{marginLeft:4}}>
                                                   <img src={`${image6}`} className="friendimage" alt="" />
-                                                  <p style={{marginTop:-2}}>Samn</p>
+                                                  <p style={{marginTop:-2}}>Success</p>
                                         </div>
                                         <div style={{marginLeft:4}}>
                                                   <img src={`${image4}`} className="friendimage" alt="" />
-                                                  <p style={{marginTop:-2}}>Uman</p>
+                                                  <p style={{marginTop:-2}}>Patience</p>
                                         </div>
                                         
                                         
